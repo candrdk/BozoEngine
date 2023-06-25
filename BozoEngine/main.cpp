516de7d7c8b5f1118c29d70e88baee0034927b4e
Caleb Andreasen <caleb.andreasen@gmail.com>
2023-06-26 01:44:56 +0200

    open window, initialize volk. (probably including glfw files I dont need to but idk and it builds)

--- BozoEngine/main.cpp
+++ BozoEngine/main.cpp
@@ -1,3 +1,34 @@
+#include <GLFW/glfw3.h>
+
+#include <volk.h>
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <stdio.h>
+
 int main(int argc, char* argv[]) {
+	VkResult volkLoaded = volkInitialize();
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "BozoEngine", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	printf("%u extensions supported\n", extensionCount);
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	glm::vec4 test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
