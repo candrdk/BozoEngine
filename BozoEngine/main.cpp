182588c7fadbf573f9791e623904d2dc7b94f741
Caleb Andreasen <caleb.andreasen@gmail.com>
2023-06-27 01:34:22 +0200

    device creation

--- BozoEngine/main.cpp
+++ BozoEngine/main.cpp
@@ -24,10 +24,22 @@ typedef int64_t	i64;
 
 #include <source_location>
 #include <vulkan/vk_enum_string_helper.h>
-// Checks if condition is true. If not, message is printed along with the source location where the error occured.
-VkResult VkCheck(VkResult result, const char* message = nullptr, std::source_location location = std::source_location::current()) {
+
+#define Check(expression, message) if (!(expression)) { \
+		std::source_location check_location = std::source_location::current(); \
+		fprintf(stderr, SGR_SET_BG_RED "[CHECK]" SGR_SET_DEFAULT " %s: `" #expression "`\n\tfile: %s(%u:%u) in `%s`\n", \
+			message,							\
+			check_location.file_name(),			\
+			check_location.line(),				\
+			check_location.column(),			\
+			check_location.function_name());	\
+		abort();								\
+	}
+
+VkResult VkCheck(VkResult result, const char* message = "", std::source_location location = std::source_location::current()) {
 	if (result != VK_SUCCESS) {
-		fprintf(stderr, SGR_SET_BG_RED "[CHECK]" SGR_SET_DEFAULT ": `%s`, %s.\n\tfile: %s(%u:%u) `%s`\n", string_VkResult(result), message,
+		fprintf(stderr, SGR_SET_BG_RED "[CHECK]" SGR_SET_DEFAULT " %s: `%s`\n\tfile: %s(%u:%u) in `%s`\n", 
+			message, string_VkResult(result),
 			location.file_name(),
 			location.line(),
 			location.column(),
@@ -45,7 +57,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
 {
 	if      (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)   printf(SGR_SET_BG_RED  "[ERROR]" SGR_SET_DEFAULT "   ");
 	else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) printf(SGR_SET_BG_BLUE "[WARNING]" SGR_SET_DEFAULT " ");
-	else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) printf(SGR_SET_BG_GRAY "[VERBOSE]" SGR_SET_DEFAULT " ");
+	else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) printf("[VERBOSE] ");
 
 	if      (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT)		printf("Validation:  ");
 	else if (messageType & VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT)		printf("Performance: ");
@@ -94,6 +106,10 @@ void CleanupWindow(GLFWwindow* window) {
 
 VkInstance instance;
 VkDebugUtilsMessengerEXT debugMessenger;
+
+VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+VkDevice device;
+VkQueue graphicsQueue;
 
 void PrintAvailableVulkanExtensions() {
 	u32 extensionCount = 0;
@@ -153,13 +169,78 @@ void CreateDebugMessenger() {
 	VkCheck(vkCreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger), "Failed to create debug messenger.");
 }
 
+void CreatePhysicalDevice() {
+	u32 deviceCount = 8;
+	VkPhysicalDevice devices[8];
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+	Check(deviceCount > 0, "Failed to find GPUs with Vulkan support.");
+
+	for (u32 i = 0; i < deviceCount; i++) {
+		VkPhysicalDeviceProperties deviceProperties;
+		vkGetPhysicalDeviceProperties(devices[i], &deviceProperties);
+
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceFeatures(devices[i], &deviceFeatures);
+
+		if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+			physicalDevice = devices[i];
+			printf("   " SGR_SET_BG_GRAY "[INFO]" SGR_SET_DEFAULT " Picked GPU: %s\n", deviceProperties.deviceName);
+			break;
+		}
+	}
+
+	Check(physicalDevice != VK_NULL_HANDLE, "Failed to find a discrete GPU.");
+}
+
+u32 GetGraphicsQueueFamily() {
+	u32 queueCount = 8;
+	VkQueueFamilyProperties queues[8];
+	vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueCount, queues);
+
+	for (int i = 0; i < queueCount; i++) {
+		if (queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			return i;
+		}
+	}
+
+	Check(false, "No queue family supporting graphics found.");
+}
+
+void CreateLogicalDevice() {
+	u32 queueFamilyIndex = GetGraphicsQueueFamily();
+	float queuePriority[] = { 1.0f };
+	VkDeviceQueueCreateInfo queueCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+		.queueFamilyIndex = queueFamilyIndex,
+		.queueCount = 1,
+		.pQueuePriorities = queuePriority
+	};
+
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+
+	// Note: enabledLayer fields are deprecated, but should probably still be set here for compatibility.
+	VkDeviceCreateInfo deviceCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+		.queueCreateInfoCount = 1,
+		.pQueueCreateInfos = &queueCreateInfo,
+		.pEnabledFeatures = &deviceFeatures
+	};
+
+	VkCheck(vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device), "Failed to create logical device.");
+
+	vkGetDeviceQueue(device, queueFamilyIndex, 0, &graphicsQueue);
+}
+
 void InitVulkan() {
 	volkInitialize();
 	CreateInstance();
 	CreateDebugMessenger();
+	CreatePhysicalDevice();
+	CreateLogicalDevice();
 }
 
 void CleanupVulkan() {
+	vkDestroyDevice(device, nullptr);
 	vkDestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 	vkDestroyInstance(instance, nullptr);
 }
